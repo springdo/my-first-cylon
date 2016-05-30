6484307da39fbf43382f76e911f10c6065a7a905
springdo <donalspring@gmail.com>
2016-05-30 18:33:27 +0000

    ADD - including the api server for rest

--- cylon.js
+++ cylon.js
@@ -2,7 +2,21 @@
 
 var Cylon = require("cylon");
 
+Cylon.api("http", {
+  host: '127.0.0.1',
+  port: '8443',
+
+  auth: {
+    type: 'basic',
+    user: 'admin',
+    pass: 'pass'
+  }
+});
+
+
 Cylon.robot({
+  name: "pibot",
+
   connections: {
     raspi: { adaptor: "raspi" }
   },
@@ -11,6 +25,11 @@ Cylon.robot({
     led: { driver: "led", pin: 7 }
   },
 
+  turnOn: function(my){
+	  my.led.turnOn(function(err, val){
+		  console.log(err || val)
+	  });
+  },
   work: function(my) {
     every((1).second(), my.led.toggle);
   }
